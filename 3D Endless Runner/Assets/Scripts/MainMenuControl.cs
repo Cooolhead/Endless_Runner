470a16b24dfdd6038814df2a6326e7a5e340fcdd
Raayan Narkar <raayannarkar@Raayans-Mac.local>
2025-04-26 09:18:53 -0700

    e”

--- 3D Endless Runner/Assets/Scripts/MainMenuControl.cs
+++ 3D Endless Runner/Assets/Scripts/MainMenuControl.cs
@@ -6,6 +6,13 @@ using UnityEngine.SceneManagement;
 public class MainMenuControl : MonoBehaviour
 {
     [SerializeField] GameObject fadeOut;
+    [SerializeField] GameObject bounceText;
+    [SerializeField] GameObject bigButton;
+    [SerializeField] GameObject animCam;
+    [SerializeField] GameObject mainCam;
+    [SerializeField] GameObject menuControls;
+    [SerializeField] AudioSource buttonSelect;
+
     void Start()
     {
         
@@ -16,15 +23,34 @@ public class MainMenuControl : MonoBehaviour
         
     }
 
-    public void Startgame()
+    public void MenuBeginButton()
+    {
+        StartCoroutine(AnimCam());
+    }
+
+
+    public void StartGame()
     {
         StartCoroutine(StartButton());
     }
 
-        IEnumerator StartButton() 
-        {
-            fadeOut.SetActive(true);
-            yield return new WaitForSeconds(1);
-            SceneManager.LoadScene(1);
-        }
+    IEnumerator StartButton()
+    {
+        buttonSelect.Play();
+        fadeOut.SetActive(true);
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(1);
     }
+
+    IEnumerator AnimCam()
+    {
+        animCam.GetComponent<Animator>().Play("AnimMenuCam");
+        bounceText.SetActive(false);
+        bigButton.SetActive(false);
+        yield return new WaitForSeconds(1.5f);
+        mainCam.SetActive(true);
+        animCam.SetActive(false);
+        menuControls.SetActive(true);
+    }
+}
+
